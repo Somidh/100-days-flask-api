70b9c94b5c4b50c1f4476fe01b02535e2a09b81b
aadarsh805 <aadarshthakur80501@gmail.com>
2023-01-23 22:40:22 +0530

    test

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ def scraper(username):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
